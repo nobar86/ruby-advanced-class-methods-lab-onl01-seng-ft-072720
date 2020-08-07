854e958dddb123469df8d3087ff03510ba99308f
Michael Rabon <nobar86@gmail.com>
2020-08-07 17:37:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,12 @@ end
     result
   end
   
-  
+  def self.find_or_create_by_name(title)
+    result = self.find_by_name(title)
+    if result
+      result
+    else
+      self.create_by_name(title)
+    end
+  end
 end
